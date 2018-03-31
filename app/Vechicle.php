0909bd9200ae6dcdd7e5e0d8664866babd746dc1
z0dd <z0dd@mail.ru>
2018-03-31 22:25:48 +0300

    changes

--- app/Vechicle.php
+++ app/Vechicle.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vechicle extends Model
 {
-    //
+    public function damage()
+    {
+    	return $this->hasMany('App\CarDamage');
+    }
+
+    public function kit()
+    {
+    	return $this->hasMany('App\CarKit');
+    }
+
+    public function damages()
+    {
+    	return $this->hasManyThrough('App\Damage', 'App\CarDamage');
+    }
+
+    public function kits()
+    {
+    	return $this->hasManyThrough('App\Kit', 'App\CarKit');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware'=>['mechanics']], function(){
+	Route::get('/', 'MechanicsController@index');
+	
+});
